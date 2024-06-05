caf1c16bfd2d442f914c3039bca8fd290c306251
monjurur <md899042@gmail.com>
2024-06-05 23:32:10 +0600

    send_gift.dart update

--- lib/pages/gift/send_gift.dart
+++ lib/pages/gift/send_gift.dart
@@ -49,7 +49,6 @@ class _SendGiftPageState extends State<SendGiftPage> {
         ),
       ),
       body: SingleChildScrollView(
-
         child: SafeArea(
           child: Padding(
             padding: EdgeInsets.only(left: 20.0.sp, right: 20.sp),
@@ -62,7 +61,10 @@ class _SendGiftPageState extends State<SendGiftPage> {
                   hintText: 'Full Name',
                   enabledBorderColor: themPinkColor,
                   keyboardType: TextInputType.text,
-                  suffixIcon: const Icon(Icons.check_circle,color: Colors.green,),
+                  suffixIcon: const Icon(
+                    Icons.check_circle,
+                    color: Colors.green,
+                  ),
                 ),
                 SizedBox(
                   height: 15.h,
@@ -78,7 +80,7 @@ class _SendGiftPageState extends State<SendGiftPage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Padding(
-                      padding:  EdgeInsets.only(right: 3.0.w),
+                      padding: EdgeInsets.only(right: 3.0.w),
                       child: SizedBox(
                         //width: 60.w,
                         child: CustomButton(
@@ -103,9 +105,9 @@ class _SendGiftPageState extends State<SendGiftPage> {
                       ),
                     ),
                     Padding(
-                      padding:  EdgeInsets.only(left: 3.0.w,right: 3.w),
+                      padding: EdgeInsets.only(left: 3.0.w, right: 3.w),
                       child: SizedBox(
-                       // width: 60.w,
+                        // width: 60.w,
                         child: CustomButton(
                           onPressed: () {
                             setState(() {
@@ -128,7 +130,7 @@ class _SendGiftPageState extends State<SendGiftPage> {
                       ),
                     ),
                     Padding(
-                      padding:  EdgeInsets.only(left: 3.w,right: 3.w),
+                      padding: EdgeInsets.only(left: 3.w, right: 3.w),
                       child: SizedBox(
                         //width: 60.w,
                         child: CustomButton(
@@ -153,7 +155,9 @@ class _SendGiftPageState extends State<SendGiftPage> {
                       ),
                     ),
                     Padding(
-                      padding:  EdgeInsets.only(left: 3.0.w,),
+                      padding: EdgeInsets.only(
+                        left: 3.0.w,
+                      ),
                       child: SizedBox(
                         //width: 60.w,
                         child: CustomButton(
@@ -179,38 +183,42 @@ class _SendGiftPageState extends State<SendGiftPage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 12.h,),
+                SizedBox(
+                  height: 12.h,
+                ),
                 TextFormField(
                   controller: amountController,
                   keyboardType: TextInputType.number,
                   focusNode: FocusNode(),
                   //onFieldSubmitted: (value) => onFieldSubmitted!(value),
-                 // validator: (input) => validator!(input),
+                  // validator: (input) => validator!(input),
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.white,
                     labelText: "Or Other Amount",
-                    labelStyle: TextStyle(color:Colors.grey.shade500,fontSize: 8.sp),
+                    labelStyle:
+                        TextStyle(color: Colors.grey.shade500, fontSize: 8.sp),
                     /*hintText: "Or Other Amount",
                     hintStyle: TextStyle(color: Colors.grey.shade400),*/
-                   // prefixIcon: icon,
+                    // prefixIcon: icon,
                     icon: const Icon(Icons.euro),
                     enabledBorder: const OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color:  Colors.black26,width: 1.5),
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
+                        borderSide:
+                            BorderSide(color: Colors.black26, width: 1.5),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
                     focusedBorder: const OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: themPinkColor,width: 1.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
+                        borderSide:
+                            BorderSide(color: themPinkColor, width: 1.5),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
                   ),
-        
                 ),
-                SizedBox(height: 8.h,),
+                SizedBox(
+                  height: 8.h,
+                ),
                 Divider(),
-                SizedBox(height: 5.h,),
+                SizedBox(
+                  height: 5.h,
+                ),
                 CustomTextField(
                   controller: fromController,
                   levelText: "From",
@@ -218,130 +226,150 @@ class _SendGiftPageState extends State<SendGiftPage> {
                   enabledBorderColor: themPinkColor,
                   keyboardType: TextInputType.text,
                 ),
-                SizedBox(height: 10.h,),
+                SizedBox(
+                  height: 10.h,
+                ),
                 Text(
                   "Massage",
                   style: TextStyle(fontSize: 10.sp, color: themPinkColor),
                 ),
-                SizedBox(height: 3.h,),
+                SizedBox(
+                  height: 3.h,
+                ),
                 TextFormField(
                   controller: amountController,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 3,
-                    focusNode: FocusNode(),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  focusNode: FocusNode(),
                   //onFieldSubmitted: (value) => onFieldSubmitted!(value),
                   // validator: (input) => validator!(input),
                   decoration: const InputDecoration(
                     filled: true,
                     fillColor: Colors.white,
-        
+
                     /*hintText: "Or Other Amount",
                     hintStyle: TextStyle(color: Colors.grey.shade400),*/
                     // prefixIcon: icon,
                     //icon: const Icon(Icons.euro),
                     enabledBorder: OutlineInputBorder(
                         borderSide:
-                        BorderSide(color:  Colors.black26,width: 1.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
+                            BorderSide(color: Colors.black26, width: 1.5),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
                     focusedBorder: OutlineInputBorder(
                         borderSide:
-                        BorderSide(color: themPinkColor,width: 1.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
+                            BorderSide(color: themPinkColor, width: 1.5),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
                   ),
-        
                 ),
-                SizedBox(height: 10.h,),
+                SizedBox(
+                  height: 10.h,
+                ),
                 Text(
                   "Select Payment",
                   style: TextStyle(fontSize: 10.sp, color: themPinkColor),
                 ),
-                SizedBox(height: 3.h,),
+                SizedBox(
+                  height: 3.h,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-        
-                  SizedBox(
-                    width: 60.w,
-                    child: Column(
-                      children: [
-                        CustomButton(
-                          onPressed: () {
-                            setState(() {
-                              if(payButton1!=true){
-                                payButton1 = true;
-                                payButton2 = false;
-                                payButton3 = false;
-                              }
-                            });
-                          },
-                          icons: Icon(Icons.euro),
-                          backGroundColor: Colors.white,
-                          borderColor:
-                          payButton1 ? themPinkColor : Colors.grey.shade400,
-                          buttonText: "Pay",
-                          buttonTextSize: 10.sp,
-                          textColor: Colors.grey,
-                        ),
-                        Text("Google Pay",style: TextStyle(fontSize: 7.sp,color: Colors.grey),)
-                      ],
+                    SizedBox(
+                      width: 60.w,
+                      child: Column(
+                        children: [
+                          CustomButton(
+                            onPressed: () {
+                              setState(() {
+                                if (payButton1 != true) {
+                                  payButton1 = true;
+                                  payButton2 = false;
+                                  payButton3 = false;
+                                }
+                              });
+                            },
+                            icons: Icon(Icons.euro),
+                            backGroundColor: Colors.white,
+                            borderColor: payButton1
+                                ? themPinkColor
+                                : Colors.grey.shade400,
+                            buttonText: "Pay",
+                            buttonTextSize: 10.sp,
+                            textColor: Colors.grey,
+                          ),
+                          Text(
+                            "Google Pay",
+                            style:
+                                TextStyle(fontSize: 7.sp, color: Colors.grey),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 60.w,
-                    child: Column(
-                      children: [
-                        CustomButton(
-                          onPressed: () {
-                            setState(() {
-                              if(payButton2!=true){
-                                payButton1 = false;
-                                payButton2 = true;
-                                payButton3 = false;
-                              }
-                            });
-                          },
-                          icons: Icon(Icons.euro),
-                          backGroundColor: Colors.white,
-                          borderColor:
-                          payButton2 ? themPinkColor : Colors.grey.shade400,
-                          buttonText: "Pay",
-                          buttonTextSize: 10.sp,
-                          textColor: Colors.grey,
-                        ),
-                        Text("Apple Pay",style: TextStyle(fontSize: 7.sp,color: Colors.grey),)
-                      ],
+                    SizedBox(
+                      width: 60.w,
+                      child: Column(
+                        children: [
+                          CustomButton(
+                            onPressed: () {
+                              setState(() {
+                                if (payButton2 != true) {
+                                  payButton1 = false;
+                                  payButton2 = true;
+                                  payButton3 = false;
+                                }
+                              });
+                            },
+                            icons: Icon(Icons.euro),
+                            backGroundColor: Colors.white,
+                            borderColor: payButton2
+                                ? themPinkColor
+                                : Colors.grey.shade400,
+                            buttonText: "Pay",
+                            buttonTextSize: 10.sp,
+                            textColor: Colors.grey,
+                          ),
+                          Text(
+                            "Apple Pay",
+                            style:
+                                TextStyle(fontSize: 7.sp, color: Colors.grey),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 60.w,
-                    child: Column(
-                      children: [
-                        CustomButton(
-                          onPressed: () {
-                            setState(() {
-                              if(payButton3!=true){
-                                payButton1 = false;
-                                payButton2 = false;
-                                payButton3 = true;
-                              }
-                            });
-                          },
-                          icons: Icon(Icons.euro),
-                          backGroundColor: Colors.white,
-                          borderColor:
-                          payButton3 ? themPinkColor : Colors.grey.shade400,
-                          //buttonText: "Pay",
-                          buttonTextSize: 10.sp,
-                          textColor: Colors.grey,
-                        ),
-                        Text("Credit/Debit Cart",style: TextStyle(fontSize: 7.sp,color: Colors.grey),)
-                      ],
+                    SizedBox(
+                      width: 60.w,
+                      child: Column(
+                        children: [
+                          CustomButton(
+                            onPressed: () {
+                              setState(() {
+                                if (payButton3 != true) {
+                                  payButton1 = false;
+                                  payButton2 = false;
+                                  payButton3 = true;
+                                }
+                              });
+                            },
+                            icons: Icon(Icons.euro),
+                            backGroundColor: Colors.white,
+                            borderColor: payButton3
+                                ? themPinkColor
+                                : Colors.grey.shade400,
+                            //buttonText: "Pay",
+                            buttonTextSize: 10.sp,
+                            textColor: Colors.grey,
+                          ),
+                          Text(
+                            "Credit/Debit Cart",
+                            style:
+                                TextStyle(fontSize: 7.sp, color: Colors.grey),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],)
+                  ],
+                )
               ],
             ),
           ),
